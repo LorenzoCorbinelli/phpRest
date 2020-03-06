2481bf1b1a53e60e5926f6a58274bc6b5b0000bc
Lorenzo <lore.corbi01@gmail.com>
2020-03-06 18:44:37 +0100

    Client and server

---
+++ classes.php
@@ -0,0 +1,40 @@
+<?php
+    $_dbHostname = "localhost:3306";
+    $_dbName = "fi_itis_meucci";
+    $_dbUsername = "root";
+    $_dbPassword = "password";
+    $_con = new PDO("mysql:host=$_dbHostname;dbname=$_dbName", $_dbUsername, $_dbPassword);
+    $_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $_con->exec('SET NAMES utf8');
+    $requestMethod = $_SERVER["REQUEST_METHOD"];
+    switch($requestMethod)
+    {
+        case 'GET':
+            $pathArray = explode('/',$_SERVER['REQUEST_URI']);
+            if(isset($pathArray[3]))
+            {
+                //con parametro id
+                $id = $pathArray[3];
+                $sql = "select * from class where id=:id";
+                $stmt = $_con->prepare($sql);
+                $params = [
+                    'id'=>$id
+                ];
+                $stmt->execute($params);
+                $data = $stmt->fetch(\PDO::FETCH_ASSOC);
+            }
+            else
+            {
+                //senza il parametro id torna tutta la tabella
+                $sql = "select * from class";
+                $stmt = $_con->prepare($sql);
+                $stmt->execute();
+                $data = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+            }
+            $js_encode = json_encode(array($data),true);
+            //output
+            header('Content-Type: application/json');
+            echo $js_encode;
+        break;
+    }
+?>
