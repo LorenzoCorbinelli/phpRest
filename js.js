1b71af48ca09ead9bd46dce8d087019844a1c08e
Lorenzo <lore.corbi01@gmail.com>
2020-03-20 15:33:40 +0100

    Client

--- js.js
+++ js.js
@@ -3,10 +3,20 @@ function scelta_tabella()
 	switch(document.getElementById('scelta_tabella').value)
 	{
 		case 'students':
+			document.getElementById('aggiungi').hidden=false;
 			get_students();
 		break;
 		case 'classes':
+			document.getElementById('aggiungi').hidden=false;
 			get_classes();
+		break;
+		case 'student_class':
+			document.getElementById('aggiungi').hidden=false;
+			get_studentClass();
+		break;
+		case 'default':
+			document.getElementById('div_tabella').innerHTML='';
+			document.getElementById('aggiungi').hidden=true;
 		break;
 	}
 }
@@ -14,7 +24,7 @@ function scelta_tabella()
 function crea_tabella_getStudents(xhr)
 {
 	var json=JSON.parse(xhr.response);
-	var table='<table class="table table-hover">';
+	var table='<table id="tabella" class="table table-hover">';
 	var th='<tr><th>ID</th><th>NAME</th><th>SURNAME</th><th>SIDICODE</th><th>TAXCODE</th><th></th></tr>';
 	table+=th;
 	json.studentInfo.forEach(function riga(item){
@@ -28,7 +38,7 @@ function crea_tabella_getStudents(xhr)
 		table+=tr;
 	});
 	table+='</table>'; 
-	document.getElementById('tabella').innerHTML=table; 
+	document.getElementById('div_tabella').innerHTML=table; 
 }
 
 function crea_tabella_getClasses(xhr)
@@ -39,14 +49,32 @@ function crea_tabella_getClasses(xhr)
 	table+=th;
 	json.classInfo.forEach(function riga(item){
 		var tr='<tr>';
-		tr+='<td>'+item.id+'</td>';
+		tr+='<tr><td>'+item.id+'</td>';
 		tr+='<td>'+item.year+'</td>';
 		tr+='<td>'+item.section+'</td>';
 		tr+='<td class="opzioni" ><img name="'+item.id+'" src="./resources/delete.svg" onclick="Delete_classes(this)">  <img src="./resources/edit.svg"></td></tr>';
 		table+=tr;
 	});
 	table+='</table>'; 
-	document.getElementById('tabella').innerHTML=table; 
+	document.getElementById('div_tabella').innerHTML=table; 
+}
+
+function crea_tabella_getStudentClass(xhr)
+{
+	var json=JSON.parse(xhr.response);
+	var table='<table class="table table-hover">';
+	var th='<tr><th>ID</th><th>ID_STUDENT</th><th>ID_CLASS</th><th></th></tr>';
+	table+=th;
+	json.student_classInfo.forEach(function riga(item){
+		var tr='<tr>';
+		tr+='<tr><td>'+item.id+'</td>';
+		tr+='<td>'+item.idStudent+'</td>';
+		tr+='<td>'+item.idClass+'</td>';
+		tr+='<td class="opzioni" ><img name="'+item.id+'" src="./resources/delete.svg" onclick="Delete_studentClass(this)">  <img src="./resources/edit.svg"></td></tr>';
+		table+=tr;
+	});
+	table+='</table>'; 
+	document.getElementById('div_tabella').innerHTML=table;
 }
 
 function Delete_classes(obj)
@@ -76,6 +104,21 @@ function Delete_students(obj)
 		alert('Errore');
 	};
 	xhr.open("DELETE", 'students.php/'+obj.name, true);
+	xhr.send();
+}
+
+function Delete_studentClass(obj)
+{
+	var xhr = new XMLHttpRequest();
+	//configuro la callback per la risposta
+	xhr.onload = function() {
+		get_studentClass();
+	};
+	//configuro la callback di errore
+	xhr.onerror = function() { 
+		alert('Errore');
+	};
+	xhr.open("DELETE", 'student_class.php/'+obj.name, true);
 	xhr.send();
 }
 
@@ -118,5 +161,20 @@ function get_classes()
 		alert('Errore');
 	};
 	xhr.open("GET", 'classes.php', true);
+	xhr.send();
+}
+
+function get_studentClass()
+{
+	var xhr = new XMLHttpRequest();
+	//configuro la callback per la risposta
+	xhr.onload = function() {
+		crea_tabella_getStudentClass(xhr);
+	};
+	//configuro la callback di errore
+	xhr.onerror = function() { 
+		alert('Errore');
+	};
+	xhr.open("GET", 'student_class.php', true);
 	xhr.send();
 }
